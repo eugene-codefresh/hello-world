478c7e8b3128a0a21dec4b7fa4d9fd92636f0537
Eugene Semirski <eugene@codefresh.io>
2017-12-01 12:05:14 +0200

    main.js modification

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
 
    // Send the response body as "Hello World"
-   response.end('Hello from Codefresh !!!\n');
+   response.end('Hello from helloworld app !!!\n');
 }).listen(8081);
 
 // Console will print the message
